871b62cd598285c163a8b6d235bd723babbfde49
meerkatjon <141840612+meerkatjon@users.noreply.github.com>
2025-10-27 17:32:15 -0400

    Update ProductPage.java

--- src/main/java/org/testingexample/pages/ProductPage.java
+++ src/main/java/org/testingexample/pages/ProductPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.openqa.selenium.JavascriptExecutor;
 import java.time.Duration;
 import java.util.List;
 
